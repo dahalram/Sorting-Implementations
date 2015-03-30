035933b28b6462e643f8b07b56edcd638efc10fa
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-03-30 16:33:03 -0400

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
-
 using namespace std;
 
-#define SIZE 5
+/*
+ * Implementation of Insertion Sort
+ * Example call:
+ *      InsertionSort::sort(myArray, SIZE);
+ */
+class InsertionSort{
+    public:
+        static void sort(int myArray[], int SIZE){
+            for (int i = 1; i < SIZE; i++){
+                int j = i;
+                while (j > 0 && (myArray[j-1] > myArray[j])){
+                    int temp = myArray[j];
+                    myArray[j] = myArray[j-1];
+                    myArray[j-1] = temp;
+                    j--;
+                }
+            }
+        }
+};
 
-void printList(int myArray[]){
-	cout << "\n Sorted List: \n";
-
-	for (int i = 0; i < SIZE; i++){
-		cout << " " << myArray[i];
-	}
-
-}
-
-void insertionSort(int myArray[]){
-
-	for (int i = 1; i < SIZE; i++){
-		int j = i;
-		while (j > 0 && (myArray[j-1] > myArray[j])){
-			int temp = myArray[j];
-			myArray[j] = myArray[j-1];
-			myArray[j-1] = temp;
-			j--;
-		}
-	}
-	printList(myArray);
-}
-
+/*
+ * Sample usage of PigeonHole Sort
+ */
 int main(){
-	int myArray[SIZE];
+    int SIZE;
+    cout << "Enter the size of the array\n";
+    cin >> SIZE;
+    cout << " Enter the "<< SIZE << " numbers to be sorted: \n" << endl;
+    int myArray[SIZE];
 
-	cout << " Enter the numbers to be sorted: \n" << endl;
+    for (int i = 0; i < SIZE; i++){
+        cin >> myArray[i];
+    }
 
-	for (int i = 0; i < SIZE; i++){
-		cin >> myArray[i];
-	}
-	insertionSort(myArray);
-	return 0;
+    InsertionSort::sort(myArray, SIZE);
+
+    //Printing the list.
+    cout << "\n Sorted List: \n";
+    for (int i = 0; i < SIZE; i++){
+        cout << " " << myArray[i];
+    }
+
+    return 0;
 }
