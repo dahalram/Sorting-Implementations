017f7fbbb0e0c3f20c98a1daf0df37365cdadcc6
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-04-04 19:26:22 -0400

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int myList[], int n){
+	int i = 0;
+	int temp;
+	bool swap = true;
+
+	while(swap){
+		swap = false;
+		i++;
+		for (int j = 0; j < n - i; j++){
+			if (myList[j] > myList[j+1]){
+				temp = myList[j];
+				myList[j] = myList[j+1];
+				myList[j+1] = temp;
+				swap = true;
+			}
+		}
+	}
+}
+
+int main(){
+	int n;
+	cout << " How many numbers do you want to sort? \n";
+	cin >> n;
+	int myList[n];
+
+	cout << " *****Enter the numbers you want to sort. *****\n";
+	for (int i = 0; i < n; i++){
+		cin >> myList[i];
+	}
+
+	bubbleSort(myList, n);
+
+	for (int i = 0; i < n; i++){
+		cout << myList[i] << " ";
+	}
+	return 0;
+}
