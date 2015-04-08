a62d3a7d93e6d65b7e048b130e8916521899ea87
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-04-07 22:46:14 -0400

    Create HeapSort.cpp

---
+++ HeapSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+void heapMax(int *myList, int i, int n){
+	int temp = myList[i], j;
+	j = 2*i;
+
+	while(j <= n){
+		if (j < n && myList[j+1] > myList[j]){
+			j++;
+		}
+		if (temp > myList[j]){
+			break;
+		}
+		else if (temp <= myList[j]){
+			myList[j/2] = myList[j];
+			j *= 2;
+		}
+	}
+	myList[j/2] = temp;
+	return;
+}
+
+void HeapSort(int *myList, int n){
+	int temp, k;
+	for (k = n; k >=2; k--){
+		temp = myList[k];
+		myList[k] = myList[1];
+		myList[1] = temp;
+		heapMax(myList, 1, k-1);
+	}
+}
+
+void makeMax(int *myList, int n){
+	for (int i = n/2; i >=1; i--){
+		heapMax(myList, i, n);
+	}
+}
+
+int main(){
+	int n;
+	cout << " Enter the number of elements to sort. \n";
+	cin >> n;
+
+	int myList[n];
+	for (int i = 1; i <= n; i++){
+		cin >> myList[i];
+	} 
+
+	makeMax(myList, n);
+	HeapSort(myList, n);
+
+	cout << " Sorted List is: \n";
+	for (int i = 1; i <= n; i++){
+		cout << myList[i] << " ";
+	}
+}
